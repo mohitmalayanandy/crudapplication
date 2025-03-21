9318bd601a1be6384eed0978690b0962196049a0
Mohit Malaya Nandy <mohitmalayanandy@gmail.com>
2025-03-21 19:42:01 +0530

    null

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  const [data, setData] = useState([])
+
+
+  const getData = () => {
+    fetch('data.json', {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      return response.json()
+    }).then((myjson) => {
+      setData(myjson)
+/*       console.log(data) */
+
+    })
+  }
+  useEffect(() => {
+    getData();
+  }, [])
+
   return (
     <div>
-      App
+      {data.map((user)=>(
+        <div key={user.id} className='flex flex-col text-center m-5 p-5'>
+          <h1>Name: {user.name}</h1>
+          <p>Username: {user.username}</p>
+          <p>E-mail: {user.email}</p>
+          <p>Mobile: {user.phone}</p>
+        </div>
+      ))}
     </div>
   )
 }
